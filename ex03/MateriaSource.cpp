962102a1c31b315abe3a0bfb1e03e46d77f0cfe4
Rita Vasques <rivasque@c3r2s6.42madrid.com>
2024-09-19 11:03:04 +0200

    final

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MateriaSource.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ritavasques <ritavasques@student.42.fr>    +#+  +:+       +#+        */
+/*   By: rivasque <rivasque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 11:27:50 by ritavasques       #+#    #+#             */
-/*   Updated: 2024/09/17 09:58:06 by ritavasques      ###   ########.fr       */
+/*   Updated: 2024/09/19 10:58:07 by rivasque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 //CONSTRUCTOR
 MateriaSource::MateriaSource( void ) {
     //std::cout << "MateriaSource defautl construstor called" << std::endl;
-     for (int i = 0; i < 4; i++)
-        this->_inventory[i] = 0;
+    for (int i = 0; i < 4; i++)
+    	this->_inventory[i] = NULL;
 }
 
 MateriaSource::MateriaSource( const MateriaSource& other ) {
@@ -35,47 +35,45 @@ MateriaSource::~MateriaSource() {
 }
 
 MateriaSource& MateriaSource::operator=( const MateriaSource& other ) {
-    for (int i = 0; i < 4; i++)
+    if (this != &other)
     {
-        if (this->_inventory[i])
-            delete this->_inventory[i];
-	    if (other._inventory[i])
-            this->_inventory[i] = other._inventory[i]->clone();
+        for (int i = 0; i < 4; i++)
+            if (this->_inventory[i])
+                delete this->_inventory[i];
+        for (int i = 0; i < 4; i++)
+                this->_inventory[i] = other._inventory[i]->clone();
     }
     return (*this); 
 }
 
 void MateriaSource::learnMateria( AMateria *m ) {
     
-    int i = 0;
-    
-    while (this->_inventory[i])
-        i++;
     if (!m)
     {
         std::cout << RED << "No materia to learn" << RESET << std::endl;
         return ;
     }
-    if (i >= 4)
-    {
-        std::cout << RED << "Cannot learn materia, inventory is full" << RESET << std::endl;
-        return ;
-    }
-    this->_inventory[i] = m;
-    std::cout << GREEN << this->_inventory[i]->getType() << ": acknowledged" << RESET << std::endl;
+    for (int i = 0; i < 4; i++)
+	{
+        if (!this->_inventory[i])
+		{
+    		this->_inventory[i] = m;
+    		std::cout << GREEN << this->_inventory[i]->getType() << ": acknowledged" << RESET << std::endl;
+            return ;
+		}
+	}
 }
 
 AMateria* MateriaSource::createMateria( std::string const & type ) {
 	
-    int i = 0;
-    
-    while (this->_inventory[i] && this->_inventory[i]->getType() != type && i < 4)
-        i++;
-    if (i < 4 && this->_inventory[i])
-    {
-        std::cout << GREEN << this->_inventory[i]->getType() << ": was created" << RESET << std::endl;
-			return (this->_inventory[i]->clone());
+    for (int i = 0; i < 4; i++)
+    { 
+        if ( this->_inventory[i] && this->_inventory[i]->getType() == type )
+        {
+			std::cout << GREEN << this->_inventory[i]->getType() << ": was created" << RESET << std::endl;
+			return (this->_inventory[i]->clone());     
+		}
     }
-    std::cout << RED << type << ": was not created" << RESET << std::endl;      
+    std::cout << RED << type << ": was not created" << RESET << std::endl; 
     return (NULL);
 }
